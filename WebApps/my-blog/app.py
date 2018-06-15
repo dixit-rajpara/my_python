b549bee31dcd989af08144fccab6c10a5293428c
Dixit Rajpara <dixit@dixit.me>
2018-06-15 10:59:11 +0530

    Removing mysql credentials.

--- WebApps/my-blog/app.py
+++ WebApps/my-blog/app.py
@@ -8,9 +8,9 @@ from functools import wraps
 app = Flask(__name__)
 
 # Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'xflow123'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flask_blog_app'
 
 # By default the db cursor returns touples as a result of query 
